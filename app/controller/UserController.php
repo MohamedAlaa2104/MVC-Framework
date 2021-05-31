3c68aeeccdc54892e1333474dd9679c8b02434f8
Mohamed Alaa <mohamedalaa2104@gmail.com>
2021-05-31 21:46:12 +0200

    Framework clean up

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -5,13 +5,9 @@ class UserController extends BaseController
 
     public function index()
     {
-        $this->userModel = $this->model('User');
         $data = [
-            'text'=>'<h1>User View Text by passing data from controller to view</h1>',
-            'users'=> $this->userModel->getUsers(),
+            'text'=>'<h1>User View</h1>',
         ];
-
-
 
         $this->view('user/index', $data);
     }
--- app/view/user/index.php
+++ app/view/user/index.php
@@ -1,13 +1,5 @@
 <?php require APPROOT . '/view/includes/header.php'; ?>
-
 <h1><?php echo $data['text']; ?></h1>
-<ul>
-<?php foreach ($data['users'] as $user): ?>
-
-    <li><?php echo $user->name ?></li>
-
-<?php endforeach ?>
-</ul>
 <?php require APPROOT . '/view/includes/footer.php'; ?>
 
 
